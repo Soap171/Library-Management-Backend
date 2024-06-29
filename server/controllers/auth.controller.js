d2dbb25b32b779f5eed95b6e3d09438218ab86dd
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2024-06-29 19:55:15 +0530

    user register api create

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,15 +1,61 @@
-export const login = async (req, res) => {
+import bcypt from "bcryptjs";
+import { errorHandle } from "../utils/error.js";
+import User from "../models/user.schema.js";
+
+export const login = async (req, res, next) => {
   res.send("This is login");
 };
 
-export const register = async (req, res) => {
-  res.send("This is signUp");
+export const register = async (req, res, next) => {
+  const {
+    username,
+    password,
+    email,
+    firstName,
+    lastName,
+    address,
+    phoneNumber,
+  } = req.body;
+
+  if (
+    !username ||
+    !password ||
+    !email ||
+    !firstName ||
+    !lastName ||
+    !address ||
+    !phoneNumber
+  ) {
+    return next(errorHandle(401, "Please fill in all fields"));
+  }
+
+  try {
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) return next(errorHandle(401, "User already exists"));
+
+    const hashedPassword = await bcypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+      email,
+      firstName,
+      lastName,
+      address,
+      phoneNumber,
+    });
+    await newUser.save();
+    return res.status(201).json(newUser);
+  } catch (error) {
+    //return next(error);
+    console.log(error);
+  }
 };
 
-export const google = async (req, res) => {
+export const google = async (req, res, next) => {
   res.send("This is Google");
 };
 
-export const logout = async (req, res) => {
+export const logout = async (req, res, next) => {
   res.send("This is log out");
 };
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { register, login, logout } from "../controllers/auth.controller.js";
+import {
+  register,
+  login,
+  logout,
+  google,
+} from "../controllers/auth.controller.js";
 
 const route = express.Router();
 
 route.post("/login", login);
 route.post("/register", register);
 route.post("/logout", logout);
-
+route.post("/google", google);
 export default route;
