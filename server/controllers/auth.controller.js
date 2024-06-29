255a48115afe41edd2848f030b1b7c96645113b9
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2024-06-29 20:07:05 +0530

    create the login api

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,9 +1,40 @@
 import bcypt from "bcryptjs";
 import { errorHandle } from "../utils/error.js";
 import User from "../models/user.schema.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
 
 export const login = async (req, res, next) => {
-  res.send("This is login");
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return next(errorHandle(401, "Please fill in all fields"));
+  }
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) return next(errorHandle(401, "Invalid username or password"));
+
+    const isMatch = await bcypt.compare(password, user.password);
+    if (!isMatch) return next(errorHandle(401, "Invalid username or password"));
+
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "2h" });
+    const { password: pass, ...rest } = user._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      })
+      .json(rest);
+  } catch (error) {
+    return next(error);
+  }
 };
 
 export const register = async (req, res, next) => {
