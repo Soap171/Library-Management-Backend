d211a3adc4a1c87738c6f7803ce4089f0f8af67e
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2024-07-05 18:39:07 +0530

    remove some console logs

--- server/utils/verifyToken.js
+++ server/utils/verifyToken.js
@@ -9,17 +9,13 @@ const JWT_SECRET_KEY = process.env.JWT_SECRET;
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
-  console.log("Token from cookies:", token); // Check if token is correctly retrieved
-
   if (!token) {
-    console.log("Token not found");
     return next(errorHandle(401, "Unauthorized"));
   }
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET_KEY);
     req.userId = decoded.userId;
-    console.log(decoded.role);
     req.userRole = decoded.role;
     console.log("Decoded userId:", req.userId, "Decoded role:", req.userRole);
     next();
