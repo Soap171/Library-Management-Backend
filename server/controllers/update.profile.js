1ade877c25a8fdea4ac222ce567e95dfa05ed293
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2024-07-05 18:02:56 +0530

    profile view api  create

--- server/controllers/update.profile.js
+++ server/controllers/update.profile.js
@@ -1,5 +1,6 @@
 import User from "../models/user.schema.js";
 import { errorHandle } from "../utils/error.js";
+import mongoose from "mongoose";
 
 export const updateProfile = async (req, res, next) => {
   const {
@@ -37,5 +38,25 @@ export const updateProfile = async (req, res, next) => {
     res.status(200).json({ message: "Profile updated successfully", userInfo });
   } catch (error) {
     return next(error);
+  }
+};
+
+export const viewProfile = async (req, res, next) => {
+  const userId = req.userId;
+  console.log(userId);
+
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return next(errorHandle(400, "Invalid user id"));
+  }
+
+  try {
+    const user = await User.findById({ _id: userId });
+    if (!user) {
+      return next(errorHandle(404, "User not found"));
+    }
+    const { password, ...userInfo } = user._doc;
+    res.status(200).json({ userInfo });
+  } catch (error) {
+    next(error);
   }
 };
