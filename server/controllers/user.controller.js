323d58cb86cb2c0e560b1f73c802326272099b07
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2024-07-06 09:04:40 +0530

    create reservation,view with view all books for user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,10 @@
 import { errorHandle } from "../utils/error.js";
 import mongoose from "mongoose";
 import Book from "../models/book.schema.js";
+import Reservation from "../models/reservation.schema.js";
+import User from "../models/user.schema.js";
 
+// search books by it's name
 export const getBookByName = async (req, res, next) => {
   const query = req.query.q;
 
@@ -24,5 +27,74 @@ export const getBookByName = async (req, res, next) => {
     res.status(200).json(books);
   } catch (error) {
     next(error); // Pass error to the global error handler
+  }
+};
+
+// view all books
+export const getAllBooks = async (req, res, next) => {
+  try {
+    const books = await Book.find();
+    if (!books) {
+      return next(errorHandle("No books found", 404));
+    }
+
+    res.status(200).json(books);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// create a reservation for a books
+export const createReservation = async (req, res, next) => {
+  const userId = req.userId; // Assuming user ID is attached to req.user
+  const { bookId } = req.body;
+
+  if (!bookId) {
+    return next(errorHandle(400, "Book ID is required"));
+  }
+
+  try {
+    const user = await User.findById(userId);
+    const book = await Book.findById(bookId);
+
+    if (!user) return next(errorHandle(404, "User not found"));
+    if (!book) return next(errorHandle(404, "Book not found"));
+
+    const reservation = new Reservation({
+      book: bookId,
+      user: userId,
+    });
+
+    await reservation.save();
+
+    user.reservations.push(reservation._id);
+    book.reservations.push(reservation._id);
+
+    await user.save();
+    await book.save();
+
+    res
+      .status(201)
+      .json({ message: "Reservation created successfully", reservation });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// view personal reservations
+export const viewReservations = async (req, res, next) => {
+  const userId = req.userId;
+
+  try {
+    const reservations = await Reservation.find({ user: userId }).populate(
+      "book",
+      "title"
+    );
+    if (!reservations.length)
+      return next(errorHandle(404, "No reservations found"));
+
+    res.status(200).json(reservations);
+  } catch (error) {
+    next(error);
   }
 };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,17 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyToken.js";
-import { getBookByName } from "../controllers/user.controller.js";
+import {
+  getAllBooks,
+  getBookByName,
+  createReservation,
+  viewReservations,
+} from "../controllers/user.controller.js";
 
 const route = express.Router();
 
 route.get("/books/search", verifyToken, getBookByName);
+route.get("/books", verifyToken, getAllBooks);
+route.post("/book/reservation", verifyToken, createReservation);
+route.get("/book/reservation", verifyToken, viewReservations);
 
 export default route;
