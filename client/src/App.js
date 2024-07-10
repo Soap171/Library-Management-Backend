c1025310f49d21340d2c309234b10e8d5796e975
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2024-07-10 21:27:06 +0530

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home.jsx";
-import Header from "./components/Header.jsx";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,93 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 function Login() {
-  return <>Login</>;
+  return (
+    <div className="container-fluid h-custom">
+      <div className="row d-flex justify-content-center align-items-center vh-100">
+        <div className="col-md-9 col-lg-6 col-xl-5">
+          <img
+            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+            className="img-fluid"
+            alt="Sample image"
+          />
+        </div>
+        <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+          <form>
+            <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start mb-2">
+              <p className="lead fw-normal mb-0 me-3">Sign in with</p>
+
+              <button
+                type="button"
+                className="btn btn-primary btn-floating mx-1"
+              >
+                <FontAwesomeIcon icon={faGoogle} />
+              </button>
+            </div>
+
+            <div className="form-outline mb-4">
+              <input
+                type="email"
+                id="email"
+                className="form-control form-control-lg"
+                placeholder="Enter a valid email address"
+              />
+              <label className="form-label" htmlFor="email">
+                Email address
+              </label>
+            </div>
+
+            <div className="form-outline mb-3">
+              <input
+                type="password"
+                id="password"
+                className="form-control form-control-lg"
+                placeholder="Enter password"
+              />
+              <label className="form-label" htmlFor="password">
+                Password
+              </label>
+            </div>
+
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="form-check mb-0">
+                <input
+                  className="form-check-input me-2"
+                  type="checkbox"
+                  value=""
+                  id="remember"
+                />
+                <label className="form-check-label" htmlFor="remember">
+                  Remember me
+                </label>
+              </div>
+              <Link to="/reset" className="text-body">
+                Forgot password?
+              </Link>
+            </div>
+
+            <div className="text-center text-lg-start mt-4 pt-2">
+              <button
+                type="button"
+                className="btn btn-primary btn-lg"
+                style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
+              >
+                Login
+              </button>
+              <p className="small fw-bold mt-2 pt-1 mb-0">
+                Don't have an account?{" "}
+                <Link to="/register" className="link-danger">
+                  Register
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
